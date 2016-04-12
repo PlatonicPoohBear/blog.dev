02a78fb881eed0454b19bf7f4a1e5b987de47ab2
PlatonicPoohBear <biologynut10@hotmail.com>
2016-04-12 10:53:36 -0500

    Added controller for simpleSimon

--- app/routes.php
+++ app/routes.php
@@ -27,6 +27,4 @@ Route::get('/rolldice/{guess}', function($guess) {
 	return View::make('roll-dice')->with($data);
 });
 
-Route::get('/simpleSimon', function() {
-	return View::make('simpleSimon');
-});
+Route::get('/simpleSimon', 'HomeController@showSimon');
