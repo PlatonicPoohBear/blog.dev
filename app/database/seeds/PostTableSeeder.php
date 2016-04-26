e770712e77218c4f3c945c646d6f1b7a0adf964e
PlatonicPoohBear <biologynut10@hotmail.com>
2016-04-26 13:22:32 -0500

    Changed seeder

--- app/database/seeds/PostTableSeeder.php
+++ app/database/seeds/PostTableSeeder.php
@@ -6,11 +6,11 @@ class PostTableSeeder extends Seeder
 	public function run()
 	{
 
-		$post = new Post();
-		$post->title = 'Hello World!';
-		$post->body = 'Test...';
-		$post->user_id = User::first()->id;
-		$post->save();
+		// $post = new Post();
+		// $post->title = 'Hello World!';
+		// $post->body = 'Test...';
+		// $post->user_id = User::first()->id;
+		// $post->save();
 	}
 }
  ?>
